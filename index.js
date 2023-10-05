0af88adc3271f15b0b7021b86771648ae6ed0ebd
Sampo Kivistö <sampo.kivisto@innofactor.com>
2023-10-05 11:28:50 +0300

    Add async overloads

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 import crypto from 'crypto';
 import swcJest from '@swc/jest';
-import {runSync} from './lib/processor.js';
+import {runSync, runAsync} from './lib/processor.js';
 
 export default {
-  process: runSync,
-  getCacheKey: function getCacheKey(
-    fileData,
-    filename,
-    { config, configString, instrument, rootDir }
-  ) {
-    return crypto
-      .createHash('md5')
-      .update(
-        swcJest.createTransformer().getCacheKey(fileData, filename, {
-          config,
-          configString,
-          instrument,
-          rootDir
-        }),
-        'hex'
-      )
-      .digest('hex')
-  },
+    process: runSync,
+    processAsync: runAsync,
+    getCacheKey: function getCacheKey(
+        fileData,
+        filename,
+        {config, configString, instrument, rootDir}
+    ) {
+        return crypto
+            .createHash('md5')
+            .update(
+                swcJest.createTransformer().getCacheKey(fileData, filename, {
+                    config,
+                    configString,
+                    instrument,
+                    rootDir
+                }),
+                'hex'
+            )
+            .digest('hex')
+    },
 };
